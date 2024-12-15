a90eece104f28cf1869552f0fa7e8ad0de4be6a7
syafiq <iqbal.syafiq.rozaan@gmail.com>
2024-12-15 17:45:29 +0700

    feat: progress bar

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -2,11 +2,58 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import {COLORS} from '../../constants';
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const END_RADIUS = {
+  full: {
+    "--endRadius": '4px',
+  },
+  part: {
+    "--endRadius": "4px 0 0 4px"
+  }
+}
+
+const SIZES = {
+  small: {
+    '--height': '8px',
+    '--padding': '0',
+  },
+  medium: {
+    '--height': '12px',
+    '--padding': '0',
+  },
+  large: {
+    '--height': '24px',
+    '--padding': '4px',
+  }
+}
+
+const ProgressBar = ({value, size}) => {
+  const progressState = value === 100 ? 'full' : 'part';
+  const endRadiusStyle = END_RADIUS[progressState];
+  const sizeStyle = SIZES[size];
+
+  return (
+    <Wrapper style={sizeStyle}>
+      <Progress style={endRadiusStyle} size={size}>{value}</Progress>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  border-radius: 8px;
+  padding: var(--padding);
+  background-color: ${COLORS.transparentGray15};
+`
+
+const Progress = styled.div`
+  width: ${p => p.children}%;
+  height: 100%;
+  color: transparent;
+  border-radius: var(--endRadius);
+  background-color: cornflowerblue;
+`
 
 export default ProgressBar;
