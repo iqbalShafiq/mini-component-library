c2d03ab6a42606b4d5d0ff130a6b2fe15cb60842
syafiq <iqbal.syafiq.rozaan@gmail.com>
2024-12-15 22:35:47 +0700

    feat: input with icon

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -13,7 +13,58 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const iconComponent = Icon({ id: icon, size: 16 });
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>
+        {label}
+      </VisuallyHidden>
+      <SearchIconWrapper htmlFor={label}>
+        {iconComponent}
+      </SearchIconWrapper>
+      <IconInputElem
+        id={label}
+        icon={iconComponent}
+        placeholder={placeholder}
+        width={width}
+      />
+    </Wrapper>
+  )
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: fit-content;
+  border-bottom: 1px solid ${COLORS.black};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const SearchIconWrapper = styled.label`
+  display: inline-block;
+  position: absolute;
+  left: 2px;
+  top: 7px;
+`
+
+const IconInputElem = styled.input`
+  width: ${p => p.width}px;
+  display: flex;
+  border: 0;
+  padding: 8px 12px 8px 26px;
+  
+  &::placeholder {
+    font-weight: 400;
+  }
+  
+  &:hover::placeholder {
+    color: ${COLORS.black};
+    font-weight: 700;
+  }
+`
 
 export default IconInput;
